1997cce2513fe01e246bd5de669bf1218439ec17
RichardSeverich <richard.severich@gmail.com>
2020-01-24 10:38:41 -0800

    issue/001: Add meteor depentencies

--- server/main.js
+++ server/main.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
+  // Create tasks collection.
 });
